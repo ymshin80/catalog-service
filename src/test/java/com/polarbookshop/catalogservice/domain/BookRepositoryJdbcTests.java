7aa72531e17ee05dd49d4882b0bc1de26e774744
ymshin80 <siyumy80@gmail.com>
2024-05-26 20:34:58 +0900

    5장 JDBC 테스트 코드

---
+++ src/test/java/com/polarbookshop/catalogservice/domain/BookRepositoryJdbcTests.java
@@ -0,0 +1,44 @@
+package com.polarbookshop.catalogservice.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.polarbookshop.catalogservice.config.DataConfig;
+
+@DataJdbcTest
+@Import(DataConfig.class)
+@AutoConfigureTestDatabase(
+  //테스트컨테이터를 이용해야 하기 때문에 내장 테스트 데이터베이스 사용을 비활성화한다.
+  replace = AutoConfigureTestDatabase.Replace.NONE
+)
+@ActiveProfiles("integration")
+public class BookRepositoryJdbcTests {
+
+  @Autowired
+  private BookReposiotry bookRepository;
+  
+  @Autowired
+  private JdbcAggregateTemplate jdbcAggregateTemplate;
+  
+  @Test
+  void findBookByIsbnWhenExisting() {
+    var bookIsbn = "1234561237";
+    var book = Book.of(bookIsbn, "title", "author", 12.90);
+    
+    jdbcAggregateTemplate.insert(book);
+    Optional<Book> actualBook = bookRepository.findByIsbn(bookIsbn);
+    
+    assertThat(actualBook).isPresent();
+    assertThat(actualBook.get().isbn()).isEqualTo(book.isbn());
+    
+  }
+}
